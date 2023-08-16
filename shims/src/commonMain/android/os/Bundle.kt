1a3b01005130c684dd6ef2a04b0f8e108e140586
Lukas Waslowski <cr7pt0gr4ph7@googlemail.com>
2023-08-16 14:18:37 +0000

    Initial shim for android.os.Bundle.

---
+++ shims/src/commonMain/android/os/Bundle.kt
@@ -0,0 +1,3 @@
+package android.os
+
+expect open class Bundle
---
+++ shims/src/src/jvmMain/android/os/Bundle.jvm.kt
@@ -0,0 +1,7 @@
+package android.os
+
+/**
+ * Shim implementation of
+ * [android.os.Bundle](https://developer.android.com/reference/android/os/Bundle).
+ */
+actual open class Bundle
